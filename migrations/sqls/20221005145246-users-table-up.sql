cc6bc4106682409e95525c0b4d3926d9161227d7
Amr Alaaeldin <amralaaeldin.dev@gmail.com>
2022-10-05 17:25:22 +0200

    creating users, products, orders tables

---
+++ migrations/sqls/20221005145246-users-table-up.sql
@@ -0,0 +1,1 @@
+CREATE TABLE users (id SERIAL PRIMARY KEY, fname VARCHAR(50), lname VARCHAR(50), email VARCHAR(100), password VARCHAR(100));
---
+++ migrations/sqls/20221005145309-orders-table-up.sql
@@ -0,0 +1,1 @@
+CREATE TABLE orders (id SERIAL PRIMARY KEY, product_id BIGINT REFERENCES products(id), quantity INT, user_id BIGINT REFERENCES users(id), status BOOLEAN);
