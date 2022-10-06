b14978f1bf3268ebd606191904e9af2554b9c48c
Amr Alaaeldin <amralaaeldin.dev@gmail.com>
2022-10-06 07:02:51 +0200

    updating db tables: adding orders_products table

--- migrations/sqls/20221005145309-orders-table-up.sql
+++ migrations/sqls/20221005145309-orders-table-up.sql
@@ -1,1 +1,1 @@
-CREATE TABLE orders (id SERIAL PRIMARY KEY, product_id BIGINT REFERENCES products(id), quantity INT, user_id BIGINT REFERENCES users(id), status BOOLEAN);
+CREATE TABLE orders (id SERIAL PRIMARY KEY, user_id BIGINT REFERENCES users(id), status BOOLEAN);
